184dd52f75d05cd41c998ae3b7a88bfed7223a74
noczero <zeroonetm@gmail.com>
2021-12-20 22:54:26 +0700

    implement logic for reading all sensor then predict it also add logic for watering checking

--- app/app.py
+++ app/app.py
@@ -2,34 +2,47 @@ import logging.config
 import os
 from time import sleep
 import RPi.GPIO as GPIO
+
+from app.src.utils import is_need_for_watering
 from src.sensors import Sensor
 from db.connection import Database
 from dotenv import load_dotenv
 import os
 import json
+
 load_dotenv()  # take environment variables from .env.
 
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-
 if __name__ == '__main__':
     logger.info("Plant Monitoring and Controlling Apps is starting...")
-    #my_sensor = Sensor()
-    #my_db = Database()
-    #my_db.insert_plant_data_to_mysql(my_sensor)
-    #GPIO.cleanup()
 
     try:
-         while True:
-             my_sensor = Sensor()
-             my_db = Database()
-             my_db.insert_plant_data_to_mysql(my_sensor)
-             sleep(60) # 1 minutes
+        while True:
+            # create sensor object
+            my_sensor = Sensor()
+
+            # create database object
+            my_db = Database()
+
+            # reading all sensor data
+            my_sensor.reading_all_sensors()
+
+            # insert data to database
+            result = my_db.insert_plant_data_to_mysql(my_sensor)
+
+            # check for watering
+            if is_need_for_watering(result):
+                my_sensor.control_relay("ON")
+                sleep(10)  # 10s ON
+                my_sensor.control_relay("OFF")
+
+            sleep(60)  # 1 minutes
+
 
     except KeyboardInterrupt:
-         print("Press Ctrl-C to terminate while statement")
-         pass
+        print("Press Ctrl-C to terminate while statement")
+        pass
     finally:
-         GPIO.cleanup()
-
+        GPIO.cleanup()
