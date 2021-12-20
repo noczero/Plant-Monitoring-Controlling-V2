05780903cf197b1b1dc7fe347a0bf967bc3a4576
noczero <zeroonetm@gmail.com>
2021-12-20 22:53:02 +0700

    improve sensor reading and add get single plant data which return input model schema

--- app/src/sensors.py
+++ app/src/sensors.py
@@ -20,7 +20,7 @@ i2c = board.I2C()
 
 # relay as output
 GPIO.setup(int(os.getenv('RELAY_PIN')), GPIO.OUT)  # relay
-GPIO.output(int(os.getenv('RELAY_PIN')), GPIO.HIGH) # set as OFF
+GPIO.output(int(os.getenv('RELAY_PIN')), GPIO.HIGH)  # set as OFF
 
 PLANT_LIST = json.loads(os.getenv('PLANT_LIST'))
 
@@ -38,6 +38,10 @@ class Sensor:
     """
 
     def __init__(self):
+        self.temperature = None
+        self.humidity = None
+        self.soil_moisture_list = None
+        self.light_intensity = None
         self.dht22 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, int(os.getenv('DHT22_PIN')))
         self.bh1750 = adafruit_bh1750.BH1750(i2c)
         self.ads1155 = Adafruit_ADS1x15.ADS1115(address=0x48)
@@ -45,10 +49,12 @@ class Sensor:
 
     def get_temperature(self):
         logger.debug("Get temperature")
+        self.temperature = self.dht22[1]
         return self.dht22[1]
 
     def get_humidity(self):
         logger.debug("Get Humidity")
+        self.humidity = self.dht22[0]
         return self.dht22[0]
 
     def get_soils_moisture(self):
@@ -74,12 +80,14 @@ class Sensor:
             logger.debug(f"ADC Values : {adc_value}")
 
             # convert to discreate value
-            discrete_value[i] = discrete_soil_reading(raw_analog=adc_value) # set value to LOW, NORMAL, or HIGH
+            discrete_value[i] = discrete_soil_reading(raw_analog=adc_value)  # set value to LOW, NORMAL, or HIGH
 
+        self.soil_moisture_list = discrete_value
         return discrete_value
 
     def get_light_intensity(self):
         logger.debug("Get Light Intensity")
+        self.light_intensity = self.bh1750.lux
         return self.bh1750.lux
 
     def control_relay(self, command):
@@ -94,3 +102,18 @@ class Sensor:
             logger.error("Invalid command")
 
         return True
+
+    def reading_all_sensors(self):
+        self.get_temperature()
+        self.get_humidity()
+        self.get_soils_moisture()
+        self.get_light_intensity()
+
+    def get_single_plant_data(self, plant_name, index_plant):
+        return {
+            "name": plant_name,
+            "temperature": self.temperature,
+            "humidity": self.humidity,
+            "light_intensity": self.light_intensity,
+            "soil_moisture": self.soil_moisture_list[index_plant]
+        }
